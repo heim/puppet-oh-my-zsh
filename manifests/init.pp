44c398fa9e694a0a9973f08918b75f8fc3fd34df
Andreas Heim <andreas@heim.no>
2012-04-10 13:01:08 +0200

    fixed overwriting of zshrc, skipped installscript, did it manually

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class oh_my_zsh {
       user => $name,
       command => "git clone $custom_files_repo custom",
       creates => "/home/$name/.oh-my-zsh/custom/zshrc",
-      require => [Package["git-core"], Exec["clone_oh_my_zsh"],  Exec["remove-zsh-custom-dir"]],
+      require => [Package["git-core"], Exec["clone_oh_my_zsh"], Exec["remove-zsh-custom-dir"]],
     }
 
     exec { "remove-zsh-custom-dir":
@@ -17,14 +17,14 @@ class oh_my_zsh {
       user => $name,
       command => "rm -rf /home/$name/.oh-my-zsh/custom",
       unless => "ls /home/$name/.oh-my-zsh/custom/.git",
-      require => Exec["clone_oh_my_zsh"],
+      require => Install_for_user[$name],
     }
     
     exec { "update-custom-files":
       path => "/bin:/usr/bin",
       user => $name,
       cwd => "/home/$name/.oh-my-zsh/custom",
-      command => "git pull origin master",
+      command => "git reset HEAD --hard && git pull origin master",
       require => Exec["clone-custom-oh-my-zsh"],
     }
 
@@ -46,7 +46,7 @@ class oh_my_zsh {
     exec { "chsh -s $path $name":
       path => "/bin:/usr/bin",
       unless => "grep -E '^${name}.+:${$path}$' /etc/passwd",
-             require => Package["zsh"]
+      require => Package["zsh"]
     }
 
     package { "zsh":
@@ -59,11 +59,20 @@ class oh_my_zsh {
       }
     }
 
+    exec { "copy-zshrc":
+      path => "/bin:/usr/bin",
+      cwd => "/home/$name",
+      user => $name,
+      command => "cp .oh-my-zsh/templates/zshrc.zsh-template .zshrc",
+      unless => "ls .zshrc",
+      require => Exec["clone_oh_my_zsh"],
+    }
+
     exec { "clone_oh_my_zsh":  
       path => "/bin:/usr/bin",
       cwd => "/home/$name",
       user => $name,
-      command => "curl -s https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh",
+      command => "git clone https://github.com/robbyrussell/oh-my-zsh.git /home/$name/.oh-my-zsh",
       creates => "/home/$name/.oh-my-zsh",
       require => [Package["git-core"], Package["zsh"], Package["curl"]],
     }
